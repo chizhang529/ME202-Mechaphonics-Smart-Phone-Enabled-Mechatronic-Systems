1af3dae72f3a2d60bbde88e137a8806493464327
chizhang529 <czhang94@stanford.edu>
2017-04-06 18:12:49 -0700

    Finished Login UI

--- Lab1_SmartBike/app/src/main/java/edu/stanford/me202/lab1_smartbike/LoginActivity.java
+++ Lab1_SmartBike/app/src/main/java/edu/stanford/me202/lab1_smartbike/LoginActivity.java
@@ -1,7 +1,9 @@
 package edu.stanford.me202.lab1_smartbike;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/magnoliascript.otf");
+        welcomeText.setTypeface(font);
     }
 }
