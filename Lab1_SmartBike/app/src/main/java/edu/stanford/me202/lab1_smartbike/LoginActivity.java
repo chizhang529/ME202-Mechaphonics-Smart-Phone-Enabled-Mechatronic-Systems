70dfec5deb399f51193ca8dd363f72708e8a6d13
chizhang529 <czhang94@stanford.edu>
2017-04-07 01:42:33 -0700

    Finished control activity UI, login and activity swtiching

--- Lab1_SmartBike/app/src/main/java/edu/stanford/me202/lab1_smartbike/LoginActivity.java
+++ Lab1_SmartBike/app/src/main/java/edu/stanford/me202/lab1_smartbike/LoginActivity.java
@@ -1,19 +1,48 @@
 package edu.stanford.me202.lab1_smartbike;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText usernameText;
+    private EditText passwordText;
+    private TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+        // initialize objects
+        welcomeText = (TextView) findViewById(R.id.welcomeText);
+        usernameText = (EditText) findViewById(R.id.username);
+        passwordText = (EditText) findViewById(R.id.password);
+
+        // display welcome text
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/magnoliascript.otf");
         welcomeText.setTypeface(font);
+
+        // set password invisible (dots rather than text)
+        passwordText.setTransformationMethod(new PasswordTransformationMethod());
+    }
+
+    public void login(View view) {
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (username.equals("czhang94") && password.equals("me202rocks")) {
+            Intent intent = new Intent(this, ControlActivity.class);
+            startActivity(intent);
+        } else {
+            Toast warningMessage = Toast.makeText(getApplicationContext(), "Error: incorrect username or password!", Toast.LENGTH_SHORT);
+            warningMessage.show();
+        }
     }
 }
